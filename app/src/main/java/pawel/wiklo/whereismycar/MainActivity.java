7b443238e8ca109c16c19492ecbefd60a9781893
Wiklos <wiklos24@interia.eu>
2018-12-18 20:03:55 +0100

    Removing onClick from xml, using onClickListener instead.

--- app/src/main/java/pawel/wiklo/whereismycar/MainActivity.java
+++ app/src/main/java/pawel/wiklo/whereismycar/MainActivity.java
@@ -9,12 +9,22 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tv1;
     private TextView tv2;
+
+    private Button myButton;
+
+    private View.OnClickListener awesomeOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            getLocation();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +36,11 @@ public class MainActivity extends AppCompatActivity {
 
         ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},123);
 
+        myButton = (Button)findViewById(R.id.button);
+        myButton.setOnClickListener(awesomeOnClickListener);
     }
 
-    public void akcja(View view) {
-
+    private void getLocation() {
         GPStracker g = new GPStracker(getApplicationContext());
         Location l = g.getLocation();
         if(l!=null)
